7323a592cf0e14e670046448f699cc0733aa9b3c
howlet <howlet@howletz.com>
2018-07-12 18:18:53 +0800

    first commit

---
+++ git-plugin/src/main/groovy/com/howletz/gradle/plugin/git/GitPlugin.groovy
@@ -0,0 +1,17 @@
+package com.howletz.gradle.plugin.git
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+/**
+ * @author howletz
+ * @date 2018/7/11
+ * */
+class GitPlugin implements Plugin<Project> {
+    @Override
+    void apply(Project project) {
+        def getId() {
+
+        }
+    }
+}
