8bd3ea830a3332ef7ee5c9db2baa958c81a01391
howlet <howlet@howletz.com>
2018-07-13 16:42:52 +0800

    add core code

--- git-plugin/src/main/groovy/com/howletz/gradle/plugin/git/GitPlugin.groovy
+++ git-plugin/src/main/groovy/com/howletz/gradle/plugin/git/GitPlugin.groovy
@@ -10,8 +10,11 @@ import org.gradle.api.Project
 class GitPlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
-        def getId() {
-
-        }
+        def gitShortCommitIdCmd = 'git rev-parse --short HEAD'
+        def gitLongCommitIdCmd = 'git rev-parse HEAD'
+        def gitBranchNameCmd = 'git symbolic-ref --short -q HEAD'
+        project.extensions.add('gitCommitShortId', gitShortCommitIdCmd.execute().text.replaceAll('\n', '').replace('\r', ''))
+        project.extensions.add('gitLongCommitId', gitLongCommitIdCmd.execute().text.replaceAll('\n', '').replace('\r', ''))
+        project.extensions.add('gitBranchName', gitBranchNameCmd.execute().text.replaceAll('\n', '').replace('\r', ''))
     }
 }
